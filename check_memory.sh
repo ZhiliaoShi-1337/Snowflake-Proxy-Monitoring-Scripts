0bf816ffc08dcb0b3db7c47ea5e52be85682480a
ZhiliaoShi-1337 <154981679+ZhiliaoShi-1337@users.noreply.github.com>
2023-12-28 21:52:51 +0100

    added detection of GiB and fixed sleep

--- check_memory.sh
+++ check_memory.sh
@@ -6,11 +6,19 @@ rm /<CHANGE THIS>/memory.log
         while true; do
 
         check_ram=$(free -h | grep Mem | awk -F " " '{print $4}')
+        check_ram_Gi=$(echo $check_ram | grep Gi | sed 's/Gi//')
         check_ram_Mi=$(echo $check_ram | grep Mi | sed 's/Mi//')
         check_ram_Ki=$(echo $check_ram | grep Ki | sed 's/Ki//')
 
+        if [ -z $check_ram_Gi ]; then
+                continue
+        else
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Gi GiB" >> /<CHANGE THIS>/memory.log
+                sleep 1
+        fi
+
         if [ -z $check_ram_Mi ]; then
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Mi MiB" >> /<CHANGE THIS>/memory.log
+                continue
         else
                 echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Mi MiB" >> /<CHANGE THIS>/memory.log
                 check_ram_Mi_amount=$(echo $check_ram_Mi | sed 's/Mi//')
@@ -20,19 +28,19 @@ rm /<CHANGE THIS>/memory.log
                         mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
                          /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
                 fi
+                sleep 1
         fi
 
         if [ -z $check_ram_Ki ]; then
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Ki KiB" /<CHANGE THIS>/memory.log
+                continue
         else
                 echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Ki KiB" /<CHANGE THIS>/memory.log
                 check_ram_Ki_amount=$(echo $check_ram_Ki | sed 's/Ki//')
                 echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less RAM ($check_ram_Ki_amount Ki)" >> /<CHANGE THIS>/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
                 killall snowflake-proxy
                 mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-                 /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+                /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+                sleep 1
         fi
-
-        sleep 1
 
         done
