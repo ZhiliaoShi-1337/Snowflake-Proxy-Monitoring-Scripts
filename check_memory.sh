5bd8f6a5d5a86ebe1b567255e70eb85ead9347d3
ZhiliaoShi-1337 <154981679+ZhiliaoShi-1337@users.noreply.github.com>
2023-12-31 04:01:45 +0100

    Update check_memory.sh
    insert string to replace via deploy script

--- check_memory.sh
+++ check_memory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat /<CHANGE THIS>/memory.log >> /<CHANGE THIS>/memory-logs/$(date -u +%Y-%m-%d)-memory.log
-rm /<CHANGE THIS>/memory.log
+cat user_input_logdir/snowflake/memory.log >> user_input_logdir/snowflake/memory-logs/$(date -u +%Y-%m-%d)-memory.log
+rm user_input_logdir/snowflake/memory.log
 
         while true; do
 
@@ -13,20 +13,20 @@ rm /<CHANGE THIS>/memory.log
         if [ -z $check_ram_Gi ]; then
                 continue
         else
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Gi GiB" >> /<CHANGE THIS>/memory.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Gi GiB" >> user_input_logdir/snowflake/memory.log
                 sleep 1
         fi
 
         if [ -z $check_ram_Mi ]; then
                 continue
         else
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Mi MiB" >> /<CHANGE THIS>/memory.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Mi MiB" >> user_input_logdir/snowflake/memory.log
                 check_ram_Mi_amount=$(echo $check_ram_Mi | sed 's/Mi//')
                 if [ $check_ram_Mi_amount -lt 20 ]; then
-                        echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less RAM ($check_ram_Mi_amount Mi)" >> /<CHANGE THIS>/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
+                        echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less RAM ($check_ram_Mi_amount Mi)" >> user_input_logdir/snowflake/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
                         killall snowflake-proxy
-                        mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-                         /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+                        mv user_input_logdir/snowflake/snowflake.log user_input_logdir/snowflake/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
+                         /usr/bin/snowflake-proxy > user_input_logdir/snowflake/snowflake.log 2>&1 &
                 fi
                 sleep 1
         fi
@@ -34,12 +34,12 @@ rm /<CHANGE THIS>/memory.log
         if [ -z $check_ram_Ki ]; then
                 continue
         else
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Ki KiB" /<CHANGE THIS>/memory.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) Free Ram: $check_ram_Ki KiB" user_input_logdir/snowflake/memory.log
                 check_ram_Ki_amount=$(echo $check_ram_Ki | sed 's/Ki//')
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less RAM ($check_ram_Ki_amount Ki)" >> /<CHANGE THIS>/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less RAM ($check_ram_Ki_amount Ki)" >> user_input_logdir/snowflake/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
                 killall snowflake-proxy
-                mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-                /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+                mv user_input_logdir/snowflake/snowflake.log user_input_logdir/snowflake/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
+                /usr/bin/snowflake-proxy > user_input_logdir/snowflake/snowflake.log 2>&1 &
                 sleep 1
         fi
 
