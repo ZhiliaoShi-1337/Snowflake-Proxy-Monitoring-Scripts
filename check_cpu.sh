7d9b88d498eda669d631309f9e04f176a7d5599c
ZhiliaoShi-1337 <154981679+ZhiliaoShi-1337@users.noreply.github.com>
2023-12-31 04:00:26 +0100

    Update check_cpu.sh
    insert string to replace via deploy script

--- check_cpu.sh
+++ check_cpu.sh
@@ -3,14 +3,14 @@ while true; do
         cpu_usage=$(mpstat 1 1 | awk '$12 ~ /[0-9.]+/ {print 100 - $12}' | awk -F "." '{print $1}' | sed -n "1p")
 
         if [ -z $cpu_usage ]; then
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) CPU Usage: $cpu_usage %" >> /<CHANGE THIS>/cpu.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) CPU Usage: $cpu_usage %" >> user_input_logdir/snowflake/cpu.log
         else
-                echo "$(date -u +%Y-%m-%d_%H-%M-%S) CPU Usage: $cpu_usage %" >> /<CHANGE THIS>/cpu.log
+                echo "$(date -u +%Y-%m-%d_%H-%M-%S) CPU Usage: $cpu_usage %" >> user_input_logdir/snowflake/cpu.log
                 if [ $cpu_usage -gt 90 ]; then
-                        echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less CPU ($cpu_usage)" >> /<CHANGE THIS>/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
+                        echo "$(date -u +%Y-%m-%d_%H-%M-%S) snowflake-proxy terminated due to less CPU ($cpu_usage)" >> user_input_logdir/snowflake/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
                         killall snowflake-proxy
-                        mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-                        /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+                        mv user_input_logdir/snowflake/snowflake.log user_input_logdir/snowflake/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
+                        /usr/bin/snowflake-proxy > user_input_logdir/snowflake/snowflake.log 2>&1 &
                 fi
         fi
 
