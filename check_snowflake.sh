762b7a69cf1d63f228385b5363bdef0b9ed6adc5
ZhiliaoShi-1337 <154981679+ZhiliaoShi-1337@users.noreply.github.com>
2023-12-31 04:02:26 +0100

    Update check_snowflake.sh
    insert string to replace via deploy script

--- check_snowflake.sh
+++ check_snowflake.sh
@@ -2,8 +2,8 @@
 
 snowflake_count=$(ps -ef | grep snowflake | grep -v 'grep' | grep -v 'check_snowflake' | grep -v 'check_cpu' | grep -v 'check_script_cpu_script' | grep -v 'check_memory' | grep -v 'check_script_memory_script.sh' | grep -v 'sleep' | wc -l)
 if [[ $snowflake_count -lt 1 ]]; then
-        mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-        echo "$(date -u +%Y-%m-%d_%H-%M-%S) No snowflake-proxy found, restarting snowflake-proxy" >> /<CHANGE THIS>/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
+        mv user_input_logdir/snowflake/snowflake.log user_input_logdir/snowflake/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
+        echo "$(date -u +%Y-%m-%d_%H-%M-%S) No snowflake-proxy found, restarting snowflake-proxy" >> user_input_logdir/snowflake/snowflake-error-logs/$(date -u +%Y-%m-%d)-err.log
         killall snowflake-proxy
-        /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+        /usr/bin/snowflake-proxy > user_input_logdir/snowflake/snowflake.log 2>&1 &
 fi
