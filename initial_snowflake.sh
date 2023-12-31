b67440fe92218d7b49bd8a7c11091e47a337cd58
ZhiliaoShi-1337 <154981679+ZhiliaoShi-1337@users.noreply.github.com>
2023-12-31 04:03:02 +0100

    Update initial_snowflake.sh
    insert string to replace via deploy script

--- initial_snowflake.sh
+++ initial_snowflake.sh
@@ -5,6 +5,6 @@ sleep 30
 snowflake_count=$(ps -ef | grep snowflake | grep -v 'grep' | grep -v 'initial_snowflake' | grep -v 'check_memory' | grep -v 'sleep' | wc -l)
 
 if [[ $snowflake_count -lt 1 ]]; then
-        mv /<CHANGE THIS>/snowflake.log /<CHANGE THIS>/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
-        /usr/bin/snowflake-proxy > /<CHANGE THIS>/snowflake.log 2>&1 &
+        mv user_input_logdir/snowflake/snowflake.log user_input_logdir/snowflake/snowflake-logs/$(date -u +%Y-%m-%d_%H-%M-%S)-snowflake.log
+        /usr/bin/snowflake-proxy > user_input_logdir/snowflake/snowflake.log 2>&1 &
 fi
